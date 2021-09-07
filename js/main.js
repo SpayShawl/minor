1564680d19aed14f98119936420ff3ff0b39405f
SpayShawl <44907982+SpayShawl@users.noreply.github.com>
2021-09-07 15:50:36 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
 import Controller from "./controllers/Controller";
 
-/*Dark themes*/
-import("../css/themes/dark/dark-yellow.css");
-import("../css/themes/dark/dark-blue.css");
-import("../css/themes/dark/dark-red.css");
-import("../css/themes/dark/dark-green.css");
+import("../css/themes/default.css");
+import("../css/themes/8bits.css");
 
-/*Light themes*/
-import("../css/themes/light/light-yellow.css");
-import("../css/themes/light/light-blue.css");
-import("../css/themes/light/light-red.css");
-import("../css/themes/light/light-green.css");
 
 new Controller();
